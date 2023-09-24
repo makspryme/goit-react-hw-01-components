8fb72c20f1f73f72f33e7afbc41e43bb3db77494
makspryme <makspryme17@gmail.com>
2023-09-24 23:45:30 +0300

    done

--- src/componetnts/Statistics/Statistics.js
+++ src/componetnts/Statistics/Statistics.js
@@ -1,16 +1,10 @@
 import css from './Statistics.module.css';
 import PropTypes from 'prop-types';
 
-function isTitle({ title }) {
-  if (title) {
-    return <h2 className={css.title}>Upload stats</h2>;
-  }
-}
-
 export default function Statistics({ title, stats }) {
   return (
     <section className={css.statistics}>
-      {isTitle({ title })}
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.stat_list}>
         {stats.map(file => {
